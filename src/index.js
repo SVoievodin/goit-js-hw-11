9db8d5d8983e1800c57925c5a64669efa17ce48e
Stas Voievodin <vstasv@i.ua>
2023-04-23 14:11:41 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const axios = require('axios/dist/node/axios.cjs'); 
