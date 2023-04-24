e7a22edc017b6eec7d2fc28c43fd6c6b828e5cb5
Stas Voievodin <vstasv@i.ua>
2023-04-24 18:12:42 +0300

    htnl+css

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const axios = require('axios/dist/node/axios.cjs'); 
+
+
